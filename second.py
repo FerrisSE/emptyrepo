3bce2debf0265c1e0ac1a2d6bbb6df1f17840796
FerrisSE <mohamedabusharkh@ferris.edu>
2023-02-01 21:38:03 -0500

    testing pull rebase

--- second.py
+++ second.py
@@ -1,4 +1,4 @@
 print(" git does not know automatically about remote updates")
 print ( "more prints to test git fetch")
 print ( "you can make changes locally and push them to remote")
-
+print(" remotely a change happened by opther developers")
